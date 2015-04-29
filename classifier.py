dcdffa4dd75b8b9d7d81fc44e05e40fefb50d380
Mehdi Cherti <mehdi@cherti.name>
2015-04-29 13:41:18 +0200

    nolearn

--- classifier.py
+++ classifier.py
@@ -1,50 +1,61 @@
 from sklearn.base import BaseEstimator
 
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
-from lasagne.easy import SimpleNeuralNet
-import numpy as np
-from sklearn.preprocessing import StandardScaler, Imputer
-from sklearn.pipeline import Pipeline
+
 import theano
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne.layers import DropoutLayer
+from lasagne.nonlinearities import softmax
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
 
 
 class Classifier(BaseEstimator):
 
     def __init__(self):
-
-        self.clf = Pipeline([
-#            ('log', LogScaler()),
-            ('scaler', StandardScaler()),
-            ('neuralnet', SimpleNeuralNet(nb_hidden_list=[50],
-                                          max_nb_epochs=30,
-                                          batch_size=128,
-                                          learning_rate=0.1,
-                                          momentum=0.3,
-                                          validation_set_ratio=0.1,
-                                          patience_threshold_progression_rate=0.001,
-                                          patience_stat="valid_loss",
-                                          optimization_method='rmsprop',
-                                          L1_factor=0.0001)),
-        ])
+        self.net = None
 
     def fit(self, X, y):
+
+        layers0 = [('input', InputLayer),
+                   ('dense0', DenseLayer),
+                   ('dropout', DropoutLayer),
+                   ('dense1', DenseLayer),
+                   ('output', DenseLayer)]
         X = X.astype(theano.config.floatX)
-        self.clf.fit(X, y)
+        encoder = LabelEncoder()
+        y = encoder.fit_transform(y).astype(np.int32)
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+        num_classes = len(encoder.classes_)
+        num_features = X.shape[1]
+        self.net = NeuralNet(layers=layers0,
+                             input_shape=(None, num_features),
+                             dense0_num_units=200,
+                             dropout_p=0.5,
+                             dense1_num_units=200,
+                             output_num_units=num_classes,
+                             output_nonlinearity=softmax,
+
+                             update=nesterov_momentum,
+                             update_learning_rate=0.01,
+                             update_momentum=0.9,
+
+                             eval_size=0.2,
+                             verbose=1,
+                             max_epochs=20)
+        self.net.fit(X, y)
         return self
 
     def predict(self, X):
         X = X.astype(theano.config.floatX)
-        return self.clf.predict(X)
+        return self.net.predict(X)
 
     def predict_proba(self, X):
         X = X.astype(theano.config.floatX)
-        return self.clf.predict_proba(X)
-
-class LogScaler(object):
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        return np.log(1 + X)
+        return self.net.predict_proba(X)
