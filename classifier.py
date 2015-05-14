f87fd73d41977cbbf70febb7d5be4a353f783743
Mehdi Cherti <mehdi@cherti.name>
2015-05-14 23:17:07 +0200

    lwta

--- classifier.py
+++ classifier.py
@@ -1,70 +1,48 @@
 from sklearn.base import BaseEstimator
 
-import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
-
+from lasagne.easy import SimpleNeuralNet, channel_out
+import numpy as np
+from sklearn.preprocessing import StandardScaler, Imputer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 import theano
-from lasagne.layers import DenseLayer
-from lasagne.layers import InputLayer
-from lasagne.layers import DropoutLayer
-from lasagne.nonlinearities import softmax
-from lasagne.nonlinearities import rectify
-from lasagne.updates import nesterov_momentum
-from lasagne.updates import adagrad
-from nolearn.lasagne import NeuralNet
 
 
 class Classifier(BaseEstimator):
 
     def __init__(self):
-        self.net = None
-        self.label_encoder = None
+        self.clf = Pipeline([
+            ('log', LogScaler()),
+            ('scaler', StandardScaler()),
+            ('neuralnet', SimpleNeuralNet(nb_hidden_list=[1000, 1000],
+                                          learning_rate=1.,
+                                          optimization_method='adadelta',
+                                          dropout_probs=[0.7, 0],
+                                          activations=['relu', channel_out(10)],
+                                          max_nb_epochs=30,
+                                          batch_size=256,
+                                          )),
+        ])
 
     def fit(self, X, y):
-        layers0 = [('input', InputLayer),
-                   ('dropoutf', DropoutLayer),
-                   ('dense0', DenseLayer),
-                   ('dropout', DropoutLayer),
-                   ('dense1', DenseLayer),
-                   ('output', DenseLayer)]
         X = X.astype(theano.config.floatX)
-        self.label_encoder = LabelEncoder()
-        y = self.label_encoder.fit_transform(y).astype(np.int32)
-        self.scaler = StandardScaler()
-        X = self.scaler.fit_transform(X)
-        num_classes = len(self.label_encoder.classes_)
-        num_features = X.shape[1]
-        self.net = NeuralNet(layers=layers0,
-                             input_shape=(None, num_features),
-
-                             dropoutf_p=0.197747030282547,
-                             dense0_num_units=1059,
-                             dense0_nonlinearity=rectify,
-                             dropout_p=0.6735734392807964,
-                             dense1_num_units=624,
-                             dense1_nonlinearity=rectify,
-                             output_num_units=num_classes,
-                             output_nonlinearity=softmax,
-
-                             update=adagrad,
-                             update_learning_rate=0.017019544327424047,
-
-                             eval_size=0.2,
-                             verbose=1,
-                             max_epochs=170,
-                             )
-        self.net.fit(X, y)
+        self.clf.fit(X, y)
         return self
 
     def predict(self, X):
         X = X.astype(theano.config.floatX)
-        X = self.scaler.fit_transform(X)
-        return self.label_encoder.inverse_transform(self.net.predict(X))
+        return self.clf.predict(X)
 
     def predict_proba(self, X):
         X = X.astype(theano.config.floatX)
-        X = self.scaler.fit_transform(X)
-        return self.net.predict_proba(X)
+        return self.clf.predict_proba(X)
+
+class LogScaler(object):
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return np.log(1 + X)
--- feature_extractor.py
+++ feature_extractor.py
@@ -2,14 +2,57 @@ import numpy as np
 import pandas as pd
 
 
+from sklearn.random_projection import johnson_lindenstrauss_min_dim
+from sklearn import random_projection
+
+from sklearn.mixture import GMM
+
 class FeatureExtractor(object):
 
     def __init__(self):
         pass
 
     def fit(self, X_dict, y):
-        pass
-
-    def transform(self, X_dict):
+        """
         cols = X_dict[0].keys()
-        return np.array([[instance[col] for col in cols] for instance in X_dict])
+        X = np.array([[instance[col] for col in cols] for instance in X_dict])
+        #min_dim = johnson_lindenstrauss_min_dim(n_samples=X.shape[0], eps=0.4)
+        #print("min_dim : %d" % (min_dim,))
+        min_dim = 200
+        self.rp = random_projection.GaussianRandomProjection(n_components=min_dim)
+        X_rp = self.rp.fit_transform(X)
+        self.gmms = []
+        self.classes = []
+        self.props = []
+
+        for cl in set(y):
+            gmm = GMM(n_components=10).fit(X_rp[y==cl])
+            self.gmms.append(gmm)
+            self.classes.append(cl)
+            self.props.append( np.sum(y==cl) / float(y.shape[0])  )
+        print(self.props)
+        """
+        return self
+
+    def transform(self, X_dict, test=False):
+        cols = X_dict[0].keys()
+        X = np.array([[instance[col] for col in cols] for instance in X_dict])
+        return X
+
+        """
+        if test == False:
+            X_list = []
+            y_list = []
+            for cl, gmm, prop in zip(self.classes, self.gmms, self.props):
+                nb = int(100000 * prop)
+                samples = gmm.sample(nb, random_state=1024).tolist()
+                X_list.append(samples)
+                y_list.append([cl] * nb)
+            X = np.concatenate(X_list, axis=0)
+            y = np.concatenate(y_list, axis=0)
+            return X, y
+        else:
+            cols = X_dict[0].keys()
+            X = np.array([[instance[col] for col in cols] for instance in X_dict])
+            return self.rp.transform(X), None
+        """
